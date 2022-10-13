38be309a287cf89d34a5c102381eb4f5ea27b17e
Hunter Thomas <zedsaidnumber11@gmail.com>
2022-10-13 13:48:43 -0600

    ITS STILL BROKEN GOD

--- ai_listener.py
+++ ai_listener.py
@@ -24,38 +24,49 @@ def listen_keyboard(Jet1, JetAi):
     JetAi.draw(JetAi.DATA.WIN, JetAi.DATA.COLS)
 
 def ai_move(Jet1, JetAi):
-    angle = JetAi.vect.angle_to(Jet1.vect)
-    text = Jet1.DATA.font.render(str(angle) , (0,0,0))
+    target_coord = translate(list(Jet1.rect.center))
+    current_coord = translate(list(JetAi.rect.center))
+    x_dist = target_coord[0] - current_coord[0]
+    y_dist = target_coord[1] - current_coord[1]
+    try:
+        theta = math.atan(y_dist/x_dist) * 57.2957795
+        cx, cy = (1 * math.cos(JetAi.angle/57.2957795)) + JetAi.x, -(1 * math.sin(JetAi.angle/57.2957795)) + JetAi.y
+        alpha = math.atan(cy / cx) * 57.2957795
+    except ZeroDivisionError:
+        theta = 90
+        alpha = 90
+    move_angle = 180 - (theta + alpha)
+    text = Jet1.DATA.font.render(str(move_angle) + "  " + str(JetAi.angle) , (0,0,0))
     text_rect = text[0].get_rect(center=(Jet1.DATA.SCREEN_WIDTH/2, Jet1.DATA.SCREEN_HEIGHT/2))
     Jet1.DATA.WIN.blit(text[0], text_rect)
-    if JetAi.angle + angle > 360:
-        temp = (JetAi.angle + angle) - 360
-        JetAi.angle = temp
-    elif JetAi.angle - angle < 0:
-        temp = (JetAi.angle - angle)
-        JetAi.angle = 360 - math.fabs(temp)
+    if JetAi.angle + move_angle > 360:
+        temp = (JetAi.angle + move_angle) - 360
+        JetAi.angle = 0 + temp
+    if JetAi.angle - move_angle < 0:
+        temp = JetAi.angle - move_angle
+        JetAi.angle = 360 + temp
     else:
-        JetAi.angle += angle
-    # target_point_x, target_point_y = Jet1.rect.center
-    # current_point_x, current_point_y = JetAi.rect.center
-    # dist_x = math.fabs(target_point_x - current_point_x)
-    # dist_y = math.fabs(target_point_y - current_point_y)
-    # try:
-    #     target_angle = math.atan(dist_y / dist_x) * 57.2957795
-    # except ZeroDivisionError:
-    #     target_angle = 90
-    # move_angle = JetAi.angle - (target_angle)
-    # JetAi.angle = move_angle
-    # print(target_angle)
-    # print(move_angle)
-    # print(JetAi.angle)
-    # if move_angle < 180 and move_angle > -180:
-    #     if JetAi.angle == 0:
-    #         JetAi.angle = 359
-    #     else:
-    #         JetAi.angle -= 1
-    # else:
-    #     if JetAi.angle == 360:
-    #         JetAi.angle = 1
-    #     else: 
-    #         JetAi.angle += 1
+        JetAi.angle += move_angle
+
+def translate(coords):
+    if coords[0] >= 960:
+        coords[0] =  coords[0] - 960
+    else:
+        coords[0] = -960 + coords[0]
+    if coords[1] >= 540:
+        coords[1] = coords[1] - 540
+    else:
+        coords[1] = -540 + coords[1]
+    return coords
+
+def untranslate(coords):
+    if coords[0] >= 960:
+        coords[0] =  coords[0] + 960
+    else:
+        coords[0] = -960 - coords[0]
+    if coords[1] >= 540:
+        coords[1] = coords[1] + 540
+    else:
+        coords[1] = -540 - coords[1]
+    return coords 
+
