a7ab3022a0ee63e3899a8c1b7a5e1c2c4fced945
Hunter Thomas <zedsaidnumber11@gmail.com>
2022-11-07 11:11:41 -0700

    Added Heatseeking missiles within radar range

--- ai_listener.py
+++ ai_listener.py
@@ -3,8 +3,9 @@ import sys
 import missile
 import math
 import time
+import jet
 
-def listen_keyboard(Jet1, JetAi):
+def listen_keyboard(Jet1: jet.Jet, JetAi: jet.Jet):
     """Function that checks for keyboard inputs and handles missile update tracking
 
     Args:
@@ -41,7 +42,7 @@ def listen_keyboard(Jet1, JetAi):
     JetAi.draw(JetAi.DATA.WIN, JetAi.DATA.COLS)
     if key[pygame.K_SPACE]:
         if Jet1.missile_cooldown == True:
-            Jet1.missiles.append(missile.Missile(Jet1.x, Jet1.y, Jet1.angle, Jet1.DATA))
+            Jet1.missiles.append(missile.Missile(Jet1.x, Jet1.y, Jet1.angle, Jet1.DATA, Jet1.checkheatseek(JetAi)))
             Jet1.missile_cooldown_time_int = Jet1.DATA.GLOBAL_TIMER.getTime()
             Jet1.missileCooldown()
         else:
@@ -61,10 +62,12 @@ def listen_keyboard(Jet1, JetAi):
                 Jet1.missiles.pop(Jet1.missiles.index(mis))
                 continue
             if mis.check:
+                if mis.heatseek:
+                    mis.heatseeker(JetAi)
                 mis.move(Jet1.DATA.MISSILE_SPEED)
                 mis.draw(Jet1.DATA.WIN, Jet1.DATA.COLS)
                 if mis.rect.collidepoint((JetAi.rect.centerx,JetAi.rect.centery)):
-                    mis.DATA.RUNNING = False
+                    mis.DATA.RUNNING = False  # type: ignore
                     text = mis.DATA.font.render("Player 1 Wins!" , (0,0,0))
                     text_rect = text[0].get_rect(center=(mis.DATA.SCREEN_WIDTH/2, mis.DATA.SCREEN_HEIGHT/2))
                     mis.DATA.WIN.blit(text[0], text_rect)
@@ -87,10 +90,12 @@ def listen_keyboard(Jet1, JetAi):
                 JetAi.missiles.pop(JetAi.missiles.index(mis))
                 continue
             if mis.check:
+                if mis.heatseek:
+                    mis.heatseeker(Jet1)
                 mis.move(JetAi.DATA.MISSILE_SPEED)
                 mis.draw(JetAi.DATA.WIN, JetAi.DATA.COLS)
                 if mis.rect.collidepoint((Jet1.rect.centerx,Jet1.rect.centery)):
-                    mis.DATA.RUNNING = False
+                    mis.DATA.RUNNING = False  # type: ignore
                     text = mis.DATA.font.render("Computer Wins!" , (0,0,0))
                     text_rect = text[0].get_rect(center=(mis.DATA.SCREEN_WIDTH/2, mis.DATA.SCREEN_HEIGHT/2))
                     mis.DATA.WIN.blit(text[0], text_rect)
@@ -100,7 +105,7 @@ def listen_keyboard(Jet1, JetAi):
                 mis.move(0)
                 mis.draw(mis.DATA.WIN, JetAi.DATA.COLS)
 
-def ai_move(Jet1, JetAi):
+def ai_move(Jet1: jet.Jet, JetAi: jet.Jet):
     """Currently handles the next AI movement, still need to reimplement NN (Weights)
 
     Args:
@@ -112,7 +117,7 @@ def ai_move(Jet1, JetAi):
     dx, dy = x - px, y - py
     if math.hypot(dx,dy) < (360):
             if JetAi.missile_cooldown == True:
-                JetAi.missiles.append(missile.Missile(JetAi.x, JetAi.y, JetAi.angle, JetAi.DATA))
+                JetAi.missiles.append(missile.Missile(JetAi.x, JetAi.y, JetAi.angle, JetAi.DATA, JetAi.checkheatseek(Jet1)))
                 JetAi.missile_cooldown_time_int = JetAi.DATA.GLOBAL_TIMER.getTime()
                 JetAi.missileCooldown()
             else:
